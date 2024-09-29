2f4fc414e8d1c11c5338898df6c1a320e4ee18ab
Asrar Abdulrahman <asrar.u20@gmail.com>
2024-09-30 01:41:43 +0300

    clean css file

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -2,32 +2,35 @@ import { FC } from "react";
 const Auth: FC = () => {
  
   return (
-    <div className="profile-settings">
-       <div className="login-container">
+    <main className="">
+       <div className="">
         <div className="login-box bg-white dark:bg-slate-600 dark:text-white p-8 rounded-2xl shadow-lg w-80 text-center">
-            <div className="login-icon">
-                <i className="fas fa-sign-in-alt"></i>
+            <div className="text-center  flex justify-center">
+                <img className="w-12 h-12" src="/logo.png" alt="" />
             </div>
-            <h2 className=" dark:text-white">Sign in with email</h2>
-            <p className=" dark:text-white">Make a new doc to bring your words, data, and teams together. For free</p>
+            <h2 className="text-2xl mb-3 dark:text-white">Sign in with email</h2>
+            <span className="mb-6 dark:text-white">Make a new doc to bring your words, data, and teams together. For free</span>
             <div>
-                <input type="email" placeholder="Email" required></input>
+                <input className="my-1" type="email" placeholder="Email" required></input>
                 <input type="password" placeholder="Password" required></input>
                 <div className="mb-5 text-right">
-                    <a className=" dark:text-white" href="#">Forgot password?</a>
+                    <span className=" dark:text-white" >Forgot password?</span>
                 </div>
-                <button type="submit">Get Started</button>
+                <button className="p-2 bg-slate-600 text-white rounded-md" type="submit">Get Started</button>
             </div>
-            <div className="social-sign-in">
-                <p>Or sign in with</p>
-                <div className="social-buttons">
+            <div className="flex flex-col items-center justify-center h-16">
+                <span className="mb-2">Or sign in with</span>
+                <div className="flex justify-between w-1/2 ">
+                <i className="ri-google-fill cursor-pointer  hover:bg-slate-200 p-1 rounded-sm"></i>
+                <i className="ri-apple-fill cursor-pointer  hover:bg-slate-200 p-1 rounded-sm"></i>
+                <i className="ri-twitter-x-fill cursor-pointer hover:bg-slate-200 p-1 rounded-sm"></i>
                     
                 </div>
             </div>
         </div>
     </div>
     
-    </div> 
+    </main> 
 
   );
 };
