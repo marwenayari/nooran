507db43f9263c90e79ff5c9da9fcc22224209b7b
Marwen Ayari <marwenayarimail@gmail.com>
2024-11-05 11:01:11 +0100

    feat: add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from '@remix-run/dev'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { remixPWA } from '@remix-pwa/dev'
 
 export default defineConfig({
   plugins: [
@@ -11,6 +12,7 @@ export default defineConfig({
         v3_throwAbortReason: true
       }
     }),
-    tsconfigPaths()
+    tsconfigPaths(),
+    remixPWA()
   ]
 })
